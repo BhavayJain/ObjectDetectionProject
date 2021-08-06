f16ef9e5beb02376be52064371ee960fb07445cc
BhavayJain <77540919+BhavayJain@users.noreply.github.com>
2021-08-06 21:05:40 +0530

    Add files via upload

--- Ac.js
+++ Ac.js
@@ -6,13 +6,30 @@ function setup(){
     
     function draw(){
         image(img,0,0,380,380);
-    }
+        if(status !=" "){
+            r=random(255);
+            g=random(255);
+            b=random(255);
+            objectDetector.detect(img,gotResult);
+            for(i=0;i < object.length ;i++) {
+                document.getElementById("status").innerHTML= "Status: Object Detected ";
+                document.getElementById("number_of_objects").innerHTML="Number Of Objects"+object.length;
+                fill(r,g,b);
+                percent= floor(object[i].confidence *100 );
+                text(object[i].label + " " + percent + "%",object[i].x +15 , object[i].y +15);
+                noFill();
+                stroke(r,g,b);
+                rect(object[i].x,object[i].y , object[i].width, object[i].height);
+    
+            }
+        }
+        }
 
 
     function modelLoaded(){
         console.log("model Loaded!");
         status=true;
-        objectDetector.detect(img,gotResult);
+       
         
     }
     function gotResult(error,results){
@@ -22,6 +39,7 @@ function setup(){
         }
          
             console.log(results);
+            object=results;
             
             
     }
@@ -33,6 +51,7 @@ function preload(){
 
 img="";
 status="";
+object=[];
 
 function back(){
     window.location="index.html";
--- desk.js
+++ desk.js
@@ -8,7 +8,7 @@ function setup(){
     function modelLoaded(){
         console.log("model Loaded!");
         status=true;
-        objectDetector.detect(img,gotResult);
+      
         
     }
     function gotResult(error,results){
@@ -18,7 +18,7 @@ function setup(){
         }
          
             console.log(results);
-            
+            object=results;
             
     }
 
@@ -29,6 +29,7 @@ function preload(){
 
 img="";
 status="";
+object=[];
 
 function back(){
     window.location="index.html";
@@ -36,4 +37,21 @@ function back(){
 
 function draw(){
     image(img,0,0,380,380);
+    if(status !=" "){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(img,gotResult);
+        for(i=0;i < object.length ;i++) {
+            document.getElementById("status").innerHTML= "Status: Object Detected ";
+            document.getElementById("number_of_objects").innerHTML="Number Of Objects"+object.length;
+            fill(r,g,b);
+            percent= floor(object[i].confidence *100 );
+            text(object[i].label + " " + percent + "%",object[i].x +15 , object[i].y +15);
+            noFill();
+            stroke(r,g,b);
+            rect(object[i].x,object[i].y , object[i].width, object[i].height);
+
+        }
+    }
 }
